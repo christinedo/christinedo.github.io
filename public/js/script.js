6cee4fe08a1a9d41f53c9bda6866809e29347d37
Christine Do <christinevdo@gmail.com>
2015-03-18 17:29:38 -0700

    going with a div overlay instead

--- public/js/script.js
+++ public/js/script.js
@@ -1,36 +1,39 @@
 $(document).ready(function() {
 	$("#podcats").click(function(event) {
-		$("#a").load("podcats.html #project1", function(responseTxt, statusTxt, xhr){
-            if(statusTxt == "success")
-                alert("External content loaded successfully!");
-            if(statusTxt == "error")
-                alert("Error: " + xhr.status + ": " + xhr.statusText);
-        });
-	});
-	
-	$("#mwch").click(function(event) {
-		$("#a").load("podcats.html #project2", function(responseTxt, statusTxt, xhr){
-            if(statusTxt == "success")
-                alert("External content loaded successfully!");
-            if(statusTxt == "error")
-                alert("Error: " + xhr.status + ": " + xhr.statusText);
-        });
-	});
-
-	$("#leicester").click(function(event) {
-		$("#a").load("podcats.html #project3", function(responseTxt, statusTxt, xhr){
-            if(statusTxt == "success")
-                alert("External content loaded successfully!");
-            if(statusTxt == "error")
-                alert("Error: " + xhr.status + ": " + xhr.statusText);
-        });
+		$("#project1").show();
 	});
 
 	$("#p1").click(function(event) {
-		event.preventDefault();
-		$("#podcats").hide("slow");
+		$("#project1").hide();
 		alert("it works");
 	});
+
+	// $("#podcats").click(function(event) {
+	// 	$("#a").load("podcats.html #project1", function(responseTxt, statusTxt, xhr){
+ //            if(statusTxt == "success")
+ //                alert("External content loaded successfully!");
+ //            if(statusTxt == "error")
+ //                alert("Error: " + xhr.status + ": " + xhr.statusText);
+ //        });
+	// });
+	
+	// $("#mwch").click(function(event) {
+	// 	$("#a").load("podcats.html #project2", function(responseTxt, statusTxt, xhr){
+ //            if(statusTxt == "success")
+ //                alert("External content loaded successfully!");
+ //            if(statusTxt == "error")
+ //                alert("Error: " + xhr.status + ": " + xhr.statusText);
+ //        });
+	// });
+
+	// $("#leicester").click(function(event) {
+	// 	$("#a").load("podcats.html #project3", function(responseTxt, statusTxt, xhr){
+ //            if(statusTxt == "success")
+ //                alert("External content loaded successfully!");
+ //            if(statusTxt == "error")
+ //                alert("Error: " + xhr.status + ": " + xhr.statusText);
+ //        });
+	// });
 
 // function closeContent() {
 // 	$("button").click(function(event) {
