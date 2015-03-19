e19fca0aedc1513a349021c6c278699ca85a636d
Christine Do <christinevdo@gmail.com>
2015-03-18 17:59:46 -0700

    changesss

--- public/js/script.js
+++ public/js/script.js
@@ -8,6 +8,39 @@ $(document).ready(function() {
 		alert("it works");
 	});
 
+	$("#x1").click(function(event) {
+		$("#project1").hide();
+		alert("it works");
+	});
+
+	$("#mwch").click(function(event) {
+		$("#project2").show();
+	});
+
+	$("#p2").click(function(event) {
+		$("#project2").hide();
+		alert("it works");
+	});
+
+	$("#x2").click(function(event) {
+		$("#project2").hide();
+		alert("it works");
+	});
+
+	$("#leicester").click(function(event) {
+		$("#project3").show();
+	});
+
+	$("#p3").click(function(event) {
+		$("#project3").hide();
+		alert("it works");
+	});
+
+	$("#x3").click(function(event) {
+		$("#project3").hide();
+		alert("it works");
+	});
+
 	// $("#podcats").click(function(event) {
 	// 	$("#a").load("podcats.html #project1", function(responseTxt, statusTxt, xhr){
  //            if(statusTxt == "success")
