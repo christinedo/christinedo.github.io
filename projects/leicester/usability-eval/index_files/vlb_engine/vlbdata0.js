57b2a0f6ae3128f341bb2ee81c72ed2198b5b44e
Christine Do <christinevdo@gmail.com>
2015-03-20 10:59:21 -0700

    added some text

--- projects/leicester/usability-eval/index_files/vlb_engine/vlbdata0.js
+++
@@ -1,1 +0,0 @@
-jQuery(document).ready(function(){ window.Lightbox = new jQuery().visualLightbox({borderSize:10,classNames:'vlightbox0',closeLocation:'top',descSliding:true,enableRightClick:false,enableSlideshow:false,pauseLocation:'imageContainerMain',prefix:'vlb0',resizeSpeed:8,slideTime:4,startZoom:true}) });
