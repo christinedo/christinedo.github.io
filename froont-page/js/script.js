47b5fbd41c6368a9571172dafa143aeb363a5a36
Christine Do <christinevdo@gmail.com>
2015-11-15 20:46:52 -0800

    add old portfolio content

---
+++ froont-page/js/script.js
@@ -0,0 +1,99 @@
+$(document).ready(function() {
+
+// PROJECT 1
+
+	$("#podcats").click(function(event) {
+		$("#project1").fadeIn(450);
+	});
+
+	$("#p1").click(function(event) {
+		$("#project1").fadeOut(500);
+		//alert("it works");
+	});
+
+	$("#x1").click(function(event) {
+		$("#project1").fadeOut(500);
+		//alert("it works");
+	});
+
+	// $("#project1").click(function(event) {
+	// 	$("#project1").fadeOut(500);
+	// 	//alert("it works");
+	// });
+
+// PROJECT 2
+
+	$("#mwch").click(function(event) {
+		$("#project2").fadeIn(450);
+	});
+
+	$("#p2").click(function(event) {
+		$("#project2").fadeOut(500);
+		//alert("it works");
+	});
+
+	$("#x2").click(function(event) {
+		$("#project2").fadeOut(500);
+		//alert("it works");
+	});
+
+	// $("#project2").click(function(event) {
+	// 	$("#project2").fadeOut(500);
+	// 	//alert("it works");
+	// });
+
+// PROJECT 3
+
+	$("#leicester").click(function(event) {
+		$("#project3").fadeIn(450);
+	});
+
+	$("#p3").click(function(event) {
+		$("#project3").fadeOut(500);
+		//alert("it works");
+	});
+
+	$("#x3").click(function(event) {
+		$("#project3").fadeOut(500);
+		//alert("it works");
+	});
+
+	// $("#project3").click(function(event) {
+	// 	$("#project3").fadeOut(500);
+	// 	//alert("it works");
+	// });
+
+	// $("#podcats").click(function(event) {
+	// 	$("#a").load("podcats.html #project1", function(responseTxt, statusTxt, xhr){
+ //            if(statusTxt == "success")
+ //                alert("External content loaded successfully!");
+ //            if(statusTxt == "error")
+ //                alert("Error: " + xhr.status + ": " + xhr.statusText);
+ //        });
+	// });
+	
+	// $("#mwch").click(function(event) {
+	// 	$("#a").load("podcats.html #project2", function(responseTxt, statusTxt, xhr){
+ //            if(statusTxt == "success")
+ //                alert("External content loaded successfully!");
+ //            if(statusTxt == "error")
+ //                alert("Error: " + xhr.status + ": " + xhr.statusText);
+ //        });
+	// });
+
+	// $("#leicester").click(function(event) {
+	// 	$("#a").load("podcats.html #project3", function(responseTxt, statusTxt, xhr){
+ //            if(statusTxt == "success")
+ //                alert("External content loaded successfully!");
+ //            if(statusTxt == "error")
+ //                alert("Error: " + xhr.status + ": " + xhr.statusText);
+ //        });
+	// });
+
+// function closeContent() {
+// 	$("button").click(function(event) {
+// 		$("#a").hide();
+// 	});
+// }
+
+});
